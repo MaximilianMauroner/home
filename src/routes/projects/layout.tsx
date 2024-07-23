99b14915ea9205808eb917bfc7a9293a696371f1
Maximilian Mauroner <github@relay.mauroner.eu>
2024-07-23 17:30:19 +0200

    if this doesn't work im lost

--- src/routes/projects/layout.tsx
+++ src/routes/projects/layout.tsx
@@ -1,6 +1,17 @@
 import { type RequestHandler } from "@builder.io/qwik-city";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
+  cacheControl(
+    {
+      public: false,
+      maxAge: 0,
+      sMaxAge: 0,
+      staleWhileRevalidate: 0,
+      noCache: true,
+      noStore: true,
+    },
+    "CDN-Cache-Control",
+  );
   cacheControl({
     public: false,
     maxAge: 0,
