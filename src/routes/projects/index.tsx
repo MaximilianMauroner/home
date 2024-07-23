6805c908ebee012f6b6ce3fd3bbfb3775a7a3e54
Maximilian Mauroner <github@relay.mauroner.eu>
2024-07-23 16:33:31 +0200

    use with env var

--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -1,6 +1,10 @@
 import { component$ } from "@builder.io/qwik";
-import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
-import ProjectCard, { Project } from "~/components/projects/project-card";
+import {
+  RequestEvent,
+  routeLoader$,
+  type DocumentHead,
+} from "@builder.io/qwik-city";
+import ProjectCard, { type Project } from "~/components/projects/project-card";
 import { default as localProjects } from "~/data/projects.json";
 
 export const head: DocumentHead = {
@@ -14,11 +18,13 @@ export const head: DocumentHead = {
   ],
 };
 
-export const useGetProjectUpdate = routeLoader$(async () => {
+export const useGetProjectUpdate = routeLoader$(async (requestEvent) => {
+  const apiKey = requestEvent.env.get("GITHUB_TOKEN");
+
   const projects: Project[] = [];
   const promises = [];
   for (const project of localProjects) {
-    promises.push(getLastPush(project.github));
+    promises.push(getLastPush(project.github, apiKey));
     projects.push({ ...project, lastUpdate: null });
   }
 
@@ -30,12 +36,18 @@ export const useGetProjectUpdate = routeLoader$(async () => {
   return projects;
 });
 
-const getLastPush = async (url: string) => {
+const getLastPush = async (url: string, apiKey: string) => {
   // https://api.github.com/repos/MaximilianMauroner/tt-friend-dl
   // https://github.com/MaximilianMauroner/tt-friend-dl
+  if (!apiKey) return null;
+
   const apiUrl = url.replace("github.com/", "api.github.com/repos/");
 
-  const res = await fetch(apiUrl);
+  const res = await fetch(apiUrl, {
+    headers: {
+      Authorization: "Bearer " + apiKey,
+    },
+  });
   const repo = await res.json();
 
   if (repo?.pushed_at) {
@@ -47,18 +59,18 @@ const getLastPush = async (url: string) => {
 const Projects = component$(() => {
   const projects = useGetProjectUpdate();
 
-  // if (process.env.NODE_ENV !== "development") {
-  projects.value.sort((a, b) => {
-    if (a.lastUpdate && b.lastUpdate) {
-      return b.lastUpdate.localeCompare(a.lastUpdate);
-    } else if (a.images?.primary) {
-      return -1;
-    } else if (b.images?.primary) {
-      return 1;
-    }
-    return a.progress > b.progress ? -1.5 : 0.5;
-  });
-  // }
+  if (import.meta.env.PROD) {
+    projects.value.sort((a, b) => {
+      if (a.lastUpdate && b.lastUpdate) {
+        return b.lastUpdate.localeCompare(a.lastUpdate);
+      } else if (a.images?.primary) {
+        return -1.25;
+      } else if (b.images?.primary) {
+        return -0.75;
+      }
+      return a.progress > b.progress ? -1.5 : -0.5;
+    });
+  }
 
   return (
     <div class={"mt-4"}>
