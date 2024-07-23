7399ea9f73cc7fe528c9c23e13130a75f1ee2257
Maximilian Mauroner <github@relay.mauroner.eu>
2024-07-23 14:28:49 +0200

    vercel cache try

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -4,12 +4,15 @@ import type { RequestHandler } from "@builder.io/qwik-city";
 import Header from "../components/header";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
-  cacheControl({
-    // Always serve a cached response by default, up to a week stale
-    staleWhileRevalidate: 60 * 5, //60 * 24 * 7,
-    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-    maxAge: 5,
-  });
+  cacheControl(
+    {
+      // Always serve a cached response by default, up to a week stale
+      staleWhileRevalidate: 60 * 5, //60 * 24 * 7,
+      // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+      maxAge: 5,
+    },
+    "Vercel-CDN-Cache-Control",
+  );
 };
 
 export default component$(() => {
