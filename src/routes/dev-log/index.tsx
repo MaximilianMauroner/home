197ea707f606dc942c69591536c83d35b1d024a4
Maximilian Mauroner <github@relay.mauroner.eu>
2024-09-18 19:53:11 +0200

    fix

--- src/routes/dev-log/index.tsx
+++ src/routes/dev-log/index.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 import { type DocumentHead } from "@builder.io/qwik-city";
 
 export const head: DocumentHead = {
-  title: "Dev Log",
+  title: "dev log",
   meta: [
     {
       name: "description",
@@ -18,10 +18,10 @@ const Blog = component$(() => {
         <div class="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
           <div class="mx-auto mb-8 max-w-screen-sm text-center lg:mb-16">
             <h2 class="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 lg:text-4xl">
-              Dev Log
+              dev log
             </h2>
             <p class="l font-light text-gray-500 sm:text-xl">
-              Weekly summary of what i've been working on
+              weekly summary of what i've been working on
             </p>
           </div>
           <div class="grid gap-8 lg:grid-cols-2"></div>
