03a1b8eb732471efd7ee580aec3a74081967a623
Maximilian Mauroner <github@relay.mauroner.eu>
2024-10-05 11:08:03 +0200

    updated revalidation dates

--- src/routes/dev-log/layout.tsx
+++ src/routes/dev-log/layout.tsx
@@ -63,7 +63,7 @@ export default component$(() => {
   return <Slot />;
 });
 
-const revalidate = 60;
+const revalidate = 60 * 60 * 12;
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   cacheControl({
     staleWhileRevalidate: revalidate,
