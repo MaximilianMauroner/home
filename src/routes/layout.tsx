7f1b28ca6761b2927378de2e564e1588fb4810b2
Maximilian Mauroner <github@relay.mauroner.eu>
2024-11-02 22:55:43 +0100

    try

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -4,19 +4,14 @@ import type { RequestHandler } from "@builder.io/qwik-city";
 import Header from "../components/header";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
+  // Control caching for this request for best performance and to reduce hosting costs:
+  // https://qwik.dev/docs/caching/
   cacheControl({
+    // Always serve a cached response by default, up to a week stale
     staleWhileRevalidate: 60 * 60 * 24 * 7,
+    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
     maxAge: 5,
-    sMaxAge: 5,
   });
-  cacheControl(
-    {
-      staleWhileRevalidate: 60 * 60 * 24 * 7,
-      maxAge: 5,
-      sMaxAge: 5,
-    },
-    "CDN-Cache-Control",
-  );
 };
 
 export default component$(() => {
