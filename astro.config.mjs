f8ead6b968f506d7fb34f8d0ebdeae1f4a1ec4bb
Maximilian Mauroner <github@relay.mauroner.eu>
2024-11-17 10:29:13 +0100

    log finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,12 +9,15 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.mauroner.net",
   integrations: [
     tailwind({ applyBaseStyles: false }),
     react(),
+    expressiveCode(),
     mdx(),
     sitemap(),
   ],
