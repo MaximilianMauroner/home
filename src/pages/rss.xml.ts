7394bb9b8f90724761fb6a9c173d71a6596ecf50
Maximilian Mauroner <github@relay.mauroner.eu>
2025-01-10 14:47:27 +0100

    created more reusable blog component

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,18 +1,11 @@
+import { getBlogs, getLogs } from '@/utils/helpers';
 import rss from '@astrojs/rss';
 import type { APIRoute } from 'astro';
-import { getCollection } from 'astro:content';
 
 export const GET: APIRoute = async ({ site }) => {
-    let blog = await getCollection('blog');
-    blog = blog.filter((post) => post.data.published).sort((a, b) => {
-        return new Date(b.data.releaseDate).getTime() - new Date(a.data.releaseDate).getTime();
-    });
+    const blog = await getBlogs();
+    const logs = await getLogs();
 
-
-    let log = await getCollection('log');
-    log = log.filter((post) => post.data.published).sort((a, b) => {
-        return new Date(b.data.releaseDate).getTime() - new Date(a.data.releaseDate).getTime();
-    })
 
     if (!site) {
         throw new Error('No site data found');
@@ -23,7 +16,7 @@ export const GET: APIRoute = async ({ site }) => {
         description: post.data.description,
         link: `/blog/${post.id}/`,
     }))
-    log.forEach((post) => {
+    logs.forEach((post) => {
         posts.push({
             title: post.data.title,
             pubDate: post.data.releaseDate,
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,4 @@
-import type { CollectionEntry } from "astro:content";
+import { getCollection, type CollectionEntry } from "astro:content";
 
 export const calculateRelativeDate = (releaseDate: Date) => {
     const date2 = new Date();
@@ -29,4 +29,24 @@ export const getTags = (blogs: CollectionEntry<"blog">[], logs: CollectionEntry<
     }
 
     return tagCounts;
+}
+
+
+const filterFunction = (post: CollectionEntry<"blog"> | CollectionEntry<"log">) => import.meta.env.DEV || (post.data.published && post.data.releaseDate < new Date());
+
+
+export const getBlogs = async () => {
+    const blog = await getCollection('blog');
+    const filteredBlog = blog.filter(filterFunction).sort((a, b) => {
+        return new Date(b.data.releaseDate).getTime() - new Date(a.data.releaseDate).getTime();
+    });
+    return filteredBlog;
+}
+
+export const getLogs = async () => {
+    const log = await getCollection('log');
+    const filteredLog = log.filter(filterFunction).sort((a, b) => {
+        return new Date(b.data.releaseDate).getTime() - new Date(a.data.releaseDate).getTime();
+    });
+    return filteredLog;
 }
