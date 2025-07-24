cbf25deeaaa9d15cde5ff5cc7a278faee922d22f
Maximilian Mauroner <github@relay.mauroner.eu>
2025-07-24 23:08:17 +0200

    removed it again

--- src/components/tools/DatingSim.tsx
+++
@@ -1,8 +0,0 @@
-export default function DatingSim() {
-  return (
-    <div>
-      <h1>Malen nach Zahlen</h1>
-      <p>Ein lustiges Malspiel, bei dem Sie nach Zahlen ausmalen können.</p>
-    </div>
-  );
-}
